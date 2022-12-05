726a91aeecb6a34c0624e2f31d81312455e4e999
최슬기 <choisk09@gmail.com>
2022-12-05 20:36:15 +0900

    FIX: fixed the checkbox-div error

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-function Person(name, gender) {
-    let married = true;
-    this.name = name;
-    this.gender = gender;
-    this.sayHello = function () {
-        console.log('hi my name is ' + this.name);
-    }
-}
+var a = 1;
+var b = a;
 
-let person = new Person('Lee', 'male');
+console.log(a, b);
+console.log(a === b);
 
-console.log(typeof person);
-console.log(person);
-
-console.log(person.gender);
-console.log(person.married);
+a = 10;
+console.log(a, b)
+console.log(a === b);
